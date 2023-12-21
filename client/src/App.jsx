55cdcabbcb53f5dcba9e0deac15acad2e7c7a96e
eyadbinamir <eyad.y.binamir@gmail.com>
2023-12-21 05:42:52 +0200

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-
+import { motion } from "framer-motion";
 import Home from "pages/home";
 import Login from "pages/login";
 import VerifyAccount from "pages/verify-account";
@@ -15,8 +15,9 @@ import "./assets/index.css";
 import ResetPassword from "pages/reset-password";
 import SetProfile from "pages/set-profile";
 import Post from "pages/post";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { logout, setAuthStatus, setUser } from "state";
+import Loading from "components/Loading";
 
 const App = () => {
   //if user is stored in redux state, then the user is logged in
@@ -25,6 +26,8 @@ const App = () => {
   const user = useSelector((state) => state.user);
   const { isVerified } = useSelector((state) => state.authStatus);
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(true);
+  const openedSession = sessionStorage.getItem("openedSession");
   useEffect(() => {
     const API_URL = process.env.REACT_APP_API_URL;
     if (user) {
@@ -37,96 +40,117 @@ const App = () => {
         }
       });
     }
+    setTimeout(() => {
+      setIsLoading(false);
+      sessionStorage.setItem("openedSession", true);
+    }, 2200);
   }, []);
   return (
     <BrowserRouter>
-      <div className={`App ${mode} bg-100 min-h-screen`}>
-        <Header />
-        <main>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                isLoggedIn ? (
-                  <Home />
-                ) : (
-                  <Navigate to={"/login"} replace={true} />
-                )
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                isLoggedIn ? <Navigate to="/" replace={true} /> : <Login />
-              }
-            />
-            <Route
-              path="/signup"
-              element={
-                isLoggedIn ? <Navigate to="/" replace={true} /> : <Signup />
-              }
-            />
-            <Route
-              path="/reset-password"
-              element={
-                isLoggedIn ? (
-                  <Navigate to={"/"} replace={true} />
-                ) : (
-                  <ResetPassword />
-                )
-              }
-            />
-            <Route
-              path="/verify-account"
-              element={
-                true ? (
-                  <VerifyAccount />
-                ) : (
-                  <Navigate to="/set-profile" replace={true} />
-                )
-              }
-            />
-            <Route
-              path="/set-profile"
-              element={
-                isVerified && !isLoggedIn ? (
-                  <SetProfile />
-                ) : (
-                  <Navigate to="/" replace={true} />
-                )
-              }
-            />
-            <Route path="/post/:userId/:postId" element={<Post />} />
-            <Route path="/post/:userId/:postId/:commentId" element={<Post />} />
-            <Route
-              path="/notifications"
-              element={
-                isLoggedIn ? (
-                  <Notifications />
-                ) : (
-                  <Navigate to="/" replace={true} />
-                )
-              }
-            />
-            <Route
-              path="/messages"
-              element={
-                isLoggedIn ? <Messages /> : <Navigate to="/" replace={true} />
-              }
-            />
-            <Route
-              path="/saved-posts"
-              element={
-                isLoggedIn ? <SavedPosts /> : <Navigate to="/" replace={true} />
-              }
-            />
-            <Route
-              path="/profile/:id"
-              element={true ? <Profile /> : <Navigate to="/" replace={true} />}
-            />
-          </Routes>
-        </main>
-      </div>
+      {isLoading && !openedSession ? (
+        <Loading />
+      ) : (
+        <div className={`App ${mode} bg-100 min-h-screen`}>
+          <Header />
+          <motion.main
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, ease: "linear" }}
+          >
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  isLoggedIn ? (
+                    <Home />
+                  ) : (
+                    <Navigate to={"/login"} replace={true} />
+                  )
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  isLoggedIn ? <Navigate to="/" replace={true} /> : <Login />
+                }
+              />
+              <Route
+                path="/signup"
+                element={
+                  isLoggedIn ? <Navigate to="/" replace={true} /> : <Signup />
+                }
+              />
+              <Route
+                path="/reset-password"
+                element={
+                  isLoggedIn ? (
+                    <Navigate to={"/"} replace={true} />
+                  ) : (
+                    <ResetPassword />
+                  )
+                }
+              />
+              <Route
+                path="/verify-account"
+                element={
+                  true ? (
+                    <VerifyAccount />
+                  ) : (
+                    <Navigate to="/set-profile" replace={true} />
+                  )
+                }
+              />
+              <Route
+                path="/set-profile"
+                element={
+                  isVerified && !isLoggedIn ? (
+                    <SetProfile />
+                  ) : (
+                    <Navigate to="/" replace={true} />
+                  )
+                }
+              />
+              <Route path="/post/:userId/:postId" element={<Post />} />
+              <Route
+                path="/post/:userId/:postId/:commentId"
+                element={<Post />}
+              />
+              <Route
+                path="/notifications"
+                element={
+                  isLoggedIn ? (
+                    <Notifications />
+                  ) : (
+                    <Navigate to="/" replace={true} />
+                  )
+                }
+              />
+              <Route
+                path="/messages"
+                element={
+                  isLoggedIn ? <Messages /> : <Navigate to="/" replace={true} />
+                }
+              />
+              <Route
+                path="/saved-posts"
+                element={
+                  isLoggedIn ? (
+                    <SavedPosts />
+                  ) : (
+                    <Navigate to="/" replace={true} />
+                  )
+                }
+              />
+              <Route
+                path="/profile/:id"
+                element={
+                  true ? <Profile /> : <Navigate to="/" replace={true} />
+                }
+              />
+            </Routes>
+          </motion.main>
+        </div>
+      )}
     </BrowserRouter>
   );
 };
