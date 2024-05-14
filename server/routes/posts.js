bbb160df8c733be45c45dabdd75e527a90c241fa
eyadbinamir <eyad.y.binamir@gmail.com>
2024-05-14 16:19:13 +0200

    fix: show feed only to logged in users.

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,14 @@
-import express from "express";
+import { Router } from "express";
 import { getFeedPosts, getUserPosts } from "../controllers/posts.js";
 import { verifyId } from "../middleware/check.js";
 import { getPostsInfo } from "../middleware/post.js";
-const router = express.Router();
+import { verifyToken } from "../middleware/auth.js";
+const router = Router();
 //root path: /posts
 
 /*READ*/
 
-router.get("/", verifyId, getFeedPosts); // get feed posts
+router.get("/", verifyId, verifyToken, getFeedPosts); // get feed posts
 router.get("/user/", verifyId, getPostsInfo, getUserPosts); // get all user's posts by page
 
 export default router;
