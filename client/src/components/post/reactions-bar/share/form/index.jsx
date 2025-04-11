3e101e3da0f99db096641aaa2f18c512388af8a1
eyadbenaamer <eyadbenaamer@gmail.com>
2025-04-11 10:06:49 +0200

    fix: updating the posts only on the proper places

--- client/src/components/post/reactions-bar/share/form/index.jsx
+++ client/src/components/post/reactions-bar/share/form/index.jsx
@@ -1,20 +1,26 @@
 import { useContext, useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import { submit } from "./submit";
 
 import { PostContext } from "components/post";
 import { PostsContext } from "components/posts";
+import { setShowMessage } from "state";
 
 const Form = (props) => {
   const { data, setData, setIsOpened } = props;
   const [isValidPost, setIsValidPost] = useState(false);
-  const { _id: userId } = useSelector((state) => state.profile);
+  const { username: myUsername } = useSelector((state) => state.profile);
   const { _id: postId, creatorId } = useContext(PostContext);
-  const { posts, setPosts } = useContext(PostsContext);
+  const postsContext = useContext(PostsContext);
 
-  const { id: userIdParam } = useParams();
+  const posts = postsContext?.posts;
+  const setPosts = postsContext?.setPosts;
+
+  const { username: currentPageUsername } = useParams();
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (data.text != "") {
@@ -23,6 +29,7 @@ const Form = (props) => {
       setIsValidPost(false);
     }
   }, [data]);
+
   return (
     <div className="flex flex-col gap-3 w-[280px] sm:w-[500px] p-2">
       <textarea
@@ -44,11 +51,17 @@ const Form = (props) => {
         onClick={() => {
           setIsOpened(false);
           submit(data, creatorId, postId).then((response) => {
+            setData({ text: "", location: "" });
+            dispatch(setShowMessage("Post shared."));
+
             /*
             check if the current page is niether another user's page nor the home page
             if so, then the shared post will appear on the top of the existing posts
             */
-            if (!userIdParam || userId == userIdParam) {
+            if (!currentPageUsername || myUsername == currentPageUsername) {
+              if (!setPosts) {
+                return;
+              }
               if (posts) {
                 setPosts([response, ...posts]);
               } else {
